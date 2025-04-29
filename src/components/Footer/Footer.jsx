3ca67d09c0028cacc3edf822c3ebb2c22378241a
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-30 00:37:44 +0600

    updated file

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
     return (
-        <footer className="footer sm:footer-horizontal bg-base-200 text-base-content mt-10 p-10">
+        <footer className="footer sm:footer-horizontal bg-base-200 text-base-content mt-20 mb-10  p-10">
             <nav>
                 <h6 className="footer-title">Services</h6>
                 <a className="link link-hover">Branding</a>
